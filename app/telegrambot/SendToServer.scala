0ac95d39e6e10c2281466516d1c32daf59c0c717
Behru <behruz.ravshonbekovich@gmail.com>
2019-11-02 12:50:30 +0500

    **

--- app/telegrambot/SendToServer.scala
+++ app/telegrambot/SendToServer.scala
@@ -4,12 +4,12 @@ import scalaj.http.{Http, HttpOptions}
 
 object SendToServer {
   def callApiAndSendMsg(text: String, tBotUser: String, tBotToken: String, tHttpLink: String, chat_id: Long): Unit = {
-    Http(tHttpLink +"/"+ text)
+    Http(tHttpLink + "/" + text)
       .header("Content-Type", "application/json")
       .header("Charset", "UTF-8")
       .option(HttpOptions.readTimeout(10000)).asString.body.mkString("")
     try {
-      new TestImage().capturePage(tHttpLink)
+      new TestImage().capturePage(tHttpLink + "/" + text )
       new BotInitializer(tBotUser, tBotToken, tHttpLink).sendImageFromUrl(chat_id)
     } catch {
       case e: Throwable =>
